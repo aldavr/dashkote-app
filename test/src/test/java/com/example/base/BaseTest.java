f3dacb47fe1f9bf9eae9d5ddfd0659cf7f731a0b
Davron Aliyev <davron.aliyev@lexisnexisrisk.com>
2022-07-05 16:22:06 -0400

    Adding new test cases for login page

---
+++ test/src/test/java/com/example/base/BaseTest.java
@@ -0,0 +1,19 @@
+package com.example.base;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+
+
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("Before Test");
+    }
+
+    @AfterTest
+    public void afterTest(){
+        System.out.println("After Test");
+    }
+
+}
---
+++ test/src/test/java/com/example/pages/LoginPage.java
@@ -0,0 +1,8 @@
+package com.example.pages;
+
+public class LoginPage {
+
+    public void signin(){
+        System.out.println("Sign in into application.");
+    }
+}
